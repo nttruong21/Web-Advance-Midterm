f496df303ce78010e3ebebdfde6f70e27abf43bc
nttruong21 <nguyenthetruong100621@gmail.com>
2022-05-10 13:44:54 +0700

    get v1

--- src/controllers/SiteController.js
+++ src/controllers/SiteController.js
@@ -1,13 +1,30 @@
+const multiparty = require("multiparty");
+const bcrypt = require("bcryptjs");
+const multer = require("multer");
+const path = require("path");
 const User = require("../models/User");
 const {
     multipleMongooseToObject,
     mongooseToObject,
 } = require("../util/mongoose");
 
+const imageStorage = multer.diskStorage({
+    destination: "src/public/avatar",
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
+
+const imageUpload = multer({
+    storage: imageStorage,
+}).single("avatar");
+
 class SiteController {
     // [GET] /
     getIndexView(req, res, next) {
-        // console.log(req.session.username);
+        if (!req.session.user) {
+            return res.redirect("/login");
+        }
         User.find({})
             .then((users) => {
                 users = multipleMongooseToObject(users);
@@ -39,39 +56,114 @@ class SiteController {
         return res.render("register");
     }
 
+    // [GET] /logout
+    logout(req, res) {
+        req.session.destroy();
+        res.redirect("/login");
+    }
+
     // [POST] /register
-    registerNewUser(req, res, next) {
-        const file = req.file;
-        const body = req.body;
-        if (file && body) {
-            const username = body.username;
-            const password = body.password;
-            const avatar = file.filename;
-            const user = new User({ username, password, avatar });
-            user.save()
-                .then(() => {
-                    res.redirect("/login");
-                })
-                .catch(next);
-        } else {
-            res.render("register");
-        }
+    async registerNewUser(req, res, next) {
+        imageUpload(req, res, function (err) {
+            if (err instanceof multer.MulterError) {
+                // A Multer error occurred when uploading.
+                console.log(">>> Error when upload image");
+            } else if (err) {
+                // An unknown error occurred when uploading.
+                console.log(">>> Error when upload image");
+            } else {
+                const file = req.file;
+                const body = req.body;
+                if (file && body) {
+                    const username = body.username;
+                    const password = body.password;
+                    const avatar = file.filename;
+                    if (!username || username.length === 0) {
+                        res.locals.errorMessage = "Please enter your username";
+                        return res.render("register");
+                    }
+                    if (!password || password.length === 0) {
+                        res.locals.errorMessage = "Please enter your password";
+                        return res.render("register");
+                    }
+                    if (password.length < 6) {
+                        res.locals.errorMessage =
+                            "Your password must contain at least 6 characters";
+                        return res.render("register");
+                    }
+                    if (!file || file.size === 0) {
+                        res.locals.errorMessage =
+                            "Please choose image to set your avatar";
+                        return res.render("register");
+                    }
+                    User.findOne({ username })
+                        .then(async (user) => {
+                            // CHECK USERNAME EXISTED
+                            if (user) {
+                                res.locals.errorMessage =
+                                    "This username was existed, please enter a different one";
+                                return res.render("register");
+                            } else {
+                                // SAVE USER
+                                const bcryptPassword = await bcrypt.hash(
+                                    password,
+                                    8
+                                );
+                                const user = new User({
+                                    username,
+                                    password: bcryptPassword,
+                                    avatar,
+                                });
+                                user.save()
+                                    .then(() => {
+                                        res.status(302).redirect("/login");
+                                    })
+                                    .catch(next);
+                            }
+                        })
+                        .catch((error) => console.log(error));
+                } else {
+                    res.render("register");
+                }
+            }
+        });
     }
 
     // [POST] /login
     handleLogin(req, res, next) {
         // console.log(req.body);
         if (req.body) {
+            const username = req.body.username;
+            const password = req.body.password;
+            if (!username || username.length === 0) {
+                res.locals.errorMessage = "Please enter your username";
+                return res.render("login");
+            }
+            if (!password || password.length === 0) {
+                res.locals.errorMessage = "Please enter your password";
+                return res.render("login");
+            }
             User.findOne({
-                username: req.body.username,
-                password: req.body.password,
+                username,
             })
-                .then((user) => {
+                .then(async (user) => {
                     if (user) {
-                        req.session.user = user;
-                        return res.redirect("/");
+                        const bcryptResult = await bcrypt.compare(
+                            password,
+                            user.password
+                        );
+                        if (bcryptResult) {
+                            req.session.user = user;
+                            return res.redirect("/");
+                        } else {
+                            res.locals.errorMessage =
+                                "Wrong password, please check and enter again";
+                            return res.render("login");
+                        }
                     } else {
-                        res.render("login");
+                        res.locals.errorMessage =
+                            "Wrong username, please check and enter again";
+                        return res.render("login");
                     }
                 })
                 .catch((error) => {
